ffb66f7b9d454cefb1a5ec8b69bc28925888317a
kvora2 <kelvinvora21@gmail.com>
2022-09-24 20:17:22 -0400

    changes_made

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var app = express();
+
+var path = require("path");
+var HTTP_PORT = process.env.PORT || 8080;
+
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
+
+app.get("/", function(req, res){
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+app.listen(HTTP_PORT, onHttpStart);
