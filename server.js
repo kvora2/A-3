01326c0a9c79929887ff5875963a1089a2aa24e6
kvora2 <kelvinvora21@gmail.com>
2022-09-24 20:35:43 -0400

    changes_commited

--- server.js
+++ server.js
@@ -8,8 +8,14 @@ function onHttpStart(){
     console.log("Express http server listening on: " + HTTP_PORT);
 }
 
+app.use(express.static('public'));
+
 app.get("/", function(req, res){
     res.sendFile(path.join(__dirname, "./views/home.html"));
+});
+
+app.get("/about", function(req, res){
+    res.sendFile(path.join(__dirname, "./views/about.html"));
 });
 
 app.listen(HTTP_PORT, onHttpStart);
