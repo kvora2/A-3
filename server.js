7d8a44de09f2c9704e520013bb994257173908d0
kvora2 <kelvinvora21@gmail.com>
2022-10-08 12:13:29 -0400

    intial commit

--- server.js
+++ server.js
@@ -1,4 +1,16 @@
-// app url ---- https://tame-goat-housecoat.cyclic.app
+/*************************************************************************
+* BTI325– Assignment 2
+* I declare that this assignment is my own work in accordance with Seneca Academic
+Policy. No part * of this assignment has been copied manually or electronically from any
+other source
+* (including 3rd party web sites) or distributed to other students.
+*
+* Name: Kelvin Vora  Student ID: 157616210 Date: 8th Oct 2022
+*
+* Your app’s URL (from Cyclic) : https://tame-goat-housecoat.cyclic.app
+*
+**************************************************************************/
+
 var express = require("express");
 var app = express();
 
